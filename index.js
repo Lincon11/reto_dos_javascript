776393d2f7bb755bd0a14e7089133405b9b1bda9
Lincon11 <eibraham37@gmail.com>
2022-10-06 16:00:04 -0500

    Recorriendo un arrays

---
+++ index.js
@@ -0,0 +1,8 @@
+var scanf = require('scanf')
+
+let HouseOfDragon = ["Rhaenerya", "Daemon", "Viserys", "Alicent"];
+
+for (let personaje = 0; personaje < HouseOfDragon.length; personaje++) {
+    console.log(`${personaje + 1}. ${HouseOfDragon[personaje]}`)
+}
+
